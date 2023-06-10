55777d30e774f7fccf81bd923a34b830ff3c4321
vorthkor <31862750+vorthkor@users.noreply.github.com>
2023-06-10 20:08:06 -0300

    feature(website): new layout

---
+++ script.js
@@ -0,0 +1,18 @@
+let ftScript = document.getElementById("footer-script")
+
+ftScript.innerHTML = `<p>
+<a href="https://github.com/vorthkor" >
+    <img src="https://louphole.com/images/icons/Github.png" alt="github"/>
+</a> | *2023_planet_Earth* |_
+<a href="https://replit.com/@vorthkor" target="_blank">
+<img src="https://www.dockhunt.com/_next/image?url=https%3A%2F%2Fdockhunt-images.nyc3.cdn.digitaloceanspaces.com%2Fd2b7021f-197f-4fb4-aca1-ddf3fc3f136c&w=256&q=75" alt="Replit">
+</a>
+<a href="https://codepen.io/vorthkor" target="_blank">
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWgddVIxoqsZAdWjDtGYkrJUwZv4xInmNcIn32MJxGXOTs6zu8B9V3YUoLssJQn7QPRFg&usqp=CAU" alt="Codepen">
+</a>
+<a href="https://exercism.org/profiles/vorthkor" target="_blank">
+<img src="https://lh3.googleusercontent.com/E1ryB4m-jdqt-guuioTCzyXEm2N1seH60Ixu7NrkBqbXIA9TCtVQjIE-cekLa5Xn-Ef4Pe96g983NzYudXVuOJVKZQ=w128-h128-e365-rj-sc0x00ffffff" alt="Exercism"></a>
+<a href="https://vorthk.ichi.city/" target="_blank">
+<img src="https://ichi.city/banner-flat.png" alt="Ichi: Your homepage on the internet!"></a>
+</p>
+`
